6559a97dfbb63477c2ee391e530610dbae84b6dd
otaku-codes <himanshupapola.ph@gmail.com>
2024-11-04 02:54:13 +0530

    temp

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.accessToken;
 
-  if (!token) {
-    return res
-      .status(401)
-      .json({ success: false, message: "You're not authorized" });
-  }
+  // if (!token) {
+  //   return res
+  //     .status(401)
+  //     .json({ success: false, message: "You're not authorized" });
+  // }
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
     if (err) {
